e69cc78e76db354eb3f091778d8141a382725d44
Andreas Sorge <as@asorge.de>
2015-08-20 16:37:15 +0200

    Enhance NLD qstat output

--- bin/nld-qstat.sh
+++ bin/nld-qstat.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-watch -n 1 'qstat -g c -q frigg.q,skadi.q,navier.q,fulla.q; echo; echo "`qstat -s r | grep $USER | wc -l` running, `qstat -s p | grep $USER | wc -l` pending, `qstat | grep $USER | wc -l` total jobs"; echo; qstat'
+QUEUES=frigg.q,skadi.q,navier.q,fulla.q
+
+qstat -g c -q $QUEUES
+echo
+echo 'Running jobs'
+qstat -q $QUEUES -u '*' -s r | awk '{print $4}' | sort | uniq -cd | sort -rn
+echo
+echo 'Queueing jobs'
+qstat -q $QUEUES -u '*' -s p | awk '{print $4}' | sort | uniq -cd | sort -rn
+echo
+echo `qstat -s r | grep $USER | wc -l` running, `qstat -s p | grep $USER | wc -l` pending, `qstat | grep $USER | wc -l` total own jobs
+echo
+qstat
